225442a0b65003c80edd1a5147bb280b13d4404c
JHLee <bubble1234568@gmail.com>
2025-05-09 10:16:53 +0900

    Fourth commit

--- dockerfile
+++ dockerfile
@@ -2,11 +2,9 @@ FROM python:3.13.3
 
 RUN pip3 install django
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY . .
-
-WORKDIR ./django_project
 
 CMD [ "python3", "manage.py", "runserver", "0.0.0.0:8000" ]
 
--- project_grading/urls.py
+++ project_grading/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("grading/", include("grading.urls")),
+    path('', include("grading.urls")),
     path('admin/', admin.site.urls),
 ]
